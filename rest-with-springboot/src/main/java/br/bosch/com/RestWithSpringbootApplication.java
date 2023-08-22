a5cb9f5cd148a5a7121d241a047c1a0aa5d3c524
gustaVASSIO <guguzim.anacleto@gmail.com>
2023-08-22 10:11:47 -0300

    Starting Project

---
+++ rest-with-springboot/src/main/java/br/bosch/com/RestWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.bosch.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringbootApplication.class, args);
+	}
+
+}
---
+++ rest-with-springboot/src/test/java/br/bosch/com/RestWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.bosch.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
